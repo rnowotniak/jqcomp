a1f310d02e2d2174da3f917a62f83ac9fbe64e3b
Rafal Michalowicz <rmichalowicz@hotmail.com>
2008-04-20 05:25:26 +0000

    QRegister: poczatek
    git-svn-id: file:///home/rob/www/git/svn-old/jqcomp@3 a4478342-f4fd-11dc-9e01-91044b1c638e

--- QRegister.java
+++ QRegister.java
@@ -1,4 +1,64 @@
+package pl.lodz.p.ics.quantum.jqcomp;
+
+
+import org.jscience.mathematics.number.*;
+import org.jscience.mathematics.vector.*;
+
+class MoreMath {
+	static public double log2(double x){
+		return Math.log10(x)/Math.log10(2);
+	}
+}
 
 public class QRegister {
-
+	 private int size;
+	 private ComplexMatrix matrix;
+	 public QRegister(Complex[] initialValues) {
+		 ComplexVector vector = ComplexVector.valueOf(initialValues);
+		 this.size = vector.getDimension();
+		 matrix = ComplexMatrix.valueOf(vector).transpose();
+	 }
+	 public QRegister(int dim) {
+		 // TODO check dimensions
+		 ComplexVector vector;
+		 Complex[] initialValues = new Complex[dim];
+		 initialValues[0] = Complex.valueOf(1,0);
+		 for (int i=1;i<dim;i++)
+			 initialValues[i] = Complex.valueOf(0, 0); 
+		 vector = ComplexVector.valueOf(initialValues);
+		 matrix = ComplexMatrix.valueOf(vector);
+		 size = dim;
+	 }
+	 
+	 public int measure() {return 0;}
+	 
+	 
+	 // X + Y
+	 //public QRegister add(QRegister that) {
+		 // TODO wrong size exception
+		 // TODO QRegister( ComplexMatrix )
+		 // return this.matrix.plus(that.matrix);
+	 //}
+	 
+	 
+	 //public String dirac();
+	 
+	 /** Inner product  */ 
+	 public Complex inner(QRegister that) {
+		 // may raise DimensionException
+		 return this.matrix.transpose().times(that.matrix).get(0, 0);
+	 }
+	 
+	 /** Outer product */
+	 public ComplexMatrix outer(QRegister that) {
+		 // may raise DimensionException
+		 return this.matrix.times(that.matrix.transpose());
+	 }
+	 
+	 // 
+	 
+	 public String toString() {
+		 return matrix.toString();
+	 }
+	
 }
---
+++ Tester.java
@@ -0,0 +1,23 @@
+package pl.lodz.p.ics.quantum.jqcomp;
+
+import org.jscience.mathematics.number.Complex;
+
+public class Tester {
+
+	 public static void main(String []args){
+		 Complex[] test = {Complex.valueOf(0,1) ,
+				 	Complex.valueOf(1,0),
+				 	Complex.valueOf(0,0),
+				 	Complex.valueOf(0,-1)
+				 	};
+		 Complex[] test2 = { Complex.valueOf(1,1),
+				 	Complex.valueOf(1,1)
+		 };
+		 QRegister reg = new QRegister(test);
+		 QRegister reg2 = new QRegister(test2);
+		 System.out.println(reg.inner(reg));
+		 System.out.println(reg.outer(reg));
+		 System.out.println(reg);
+		 
+	 }
+}
