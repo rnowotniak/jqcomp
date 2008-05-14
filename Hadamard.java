fa4e4a031a37f1ea9615f0d29057eca3d5d307d4
Andrzej Nieradka <andnie@stud.ics.p.lodz.pl>
2008-05-14 04:23:28 +0000

    git-svn-id: file:///home/rob/www/git/svn-old/jqcomp@18 a4478342-f4fd-11dc-9e01-91044b1c638e

--- Hadamard.java
+++ Hadamard.java
@@ -1,19 +1,22 @@
 package pl.lodz.p.ics.quantum.jqcomp;
 
 import org.jscience.mathematics.vector.ComplexMatrix;
+import org.jscience.mathematics.number.Complex;
 
+// W³aœciwie nie widze potrzeby robienia oddzielnych klas pod warunkiem 
+// ¿e zachowamy 'niezmiennoœæ' QGate; mo¿na by zrobiæ sta³e statyczne.
 public class Hadamard extends AbstractQGate {
-	public Hadamard(ComplexMatrix matrix) {
-		super(matrix);
-		// TODO Auto-generated constructor stub
-	}
-
-	public Hadamard(QGate other) {
-		super(other);
-		// TODO Auto-generated constructor stub
-	}
-
 	public Hadamard() {
-		// TODO Auto-generated constructor stub
-	}
+		ComplexMatrix m = h2;
+		
+		/* hmm
+		 * for i in xrange(size - 1):
+            m = kron(m, h2)
+		 */		
+	}	
+	
+	private static final ComplexMatrix h2 = ComplexMatrix.valueOf(new Complex[][] {
+			new Complex[] {Complex.valueOf(1, 0), Complex.valueOf(1, 0)},
+			new Complex[] {Complex.valueOf(1, 0), Complex.valueOf(-1, 0)}
+	}).times(Complex.valueOf(Math.sqrt(2) / 2, 0));	
 }
