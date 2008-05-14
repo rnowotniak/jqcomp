74d18cc48f77406d6193872e9ab13d78fcfe3f0e
Andrzej Nieradka <andnie@stud.ics.p.lodz.pl>
2008-05-14 06:40:02 +0000

    Added a file remotely
    git-svn-id: file:///home/rob/www/git/svn-old/jqcomp@24 a4478342-f4fd-11dc-9e01-91044b1c638e

---
+++ Not.java
@@ -0,0 +1,21 @@
+package pl.lodz.p.ics.quantum.jqcomp;
+
+import org.jscience.mathematics.vector.ComplexMatrix;
+
+public class Not extends AbstractQGate {
+
+	public Not(ComplexMatrix matrix) {
+		super(matrix);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Not(QGate other) {
+		super(other);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Not() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
