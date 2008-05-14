dbf5f443dbf17e389e62e15ce56fa9bf54efc270
Andrzej Nieradka <andnie@stud.ics.p.lodz.pl>
2008-05-14 03:55:04 +0000

    git-svn-id: file:///home/rob/www/git/svn-old/jqcomp@9 a4478342-f4fd-11dc-9e01-91044b1c638e

---
+++ AbstractQGate.java
@@ -0,0 +1,24 @@
+package pl.lodz.p.ics.quantum.jqcomp;
+
+import org.jscience.mathematics.vector.ComplexMatrix;
+
+// jak to ma byæ?
+// class AbstractQGate(QGate):
+//		pass
+public class AbstractQGate extends QGate {
+
+	public AbstractQGate(ComplexMatrix matrix) {
+		super(matrix);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AbstractQGate(QGate other) {
+		super(other);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AbstractQGate() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
