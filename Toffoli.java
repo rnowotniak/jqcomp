914c8c37fb88e5034e5d28f1ecc1fb0f7e54046b
Andrzej Nieradka <andnie@stud.ics.p.lodz.pl>
2008-05-14 06:40:36 +0000

    Added a file remotely
    git-svn-id: file:///home/rob/www/git/svn-old/jqcomp@26 a4478342-f4fd-11dc-9e01-91044b1c638e

---
+++ Toffoli.java
@@ -0,0 +1,25 @@
+package pl.lodz.p.ics.quantum.jqcomp;
+
+import org.jscience.mathematics.vector.ComplexMatrix;
+import org.jscience.mathematics.number.Complex;
+
+
+public class Toffoli extends AbstractQGate {
+	public Toffoli() {
+		this.matrix = tmatrix;
+		this.size = tmatrix.getNumberOfRows();
+		// dlaczego tam jest size = 3 ???
+	}
+	
+	private static final ComplexMatrix tmatrix = ComplexMatrix.valueOf(
+		new Complex[][]{
+				{ cx(1),  cx(0),  cx(0),  cx(0),  cx(0),  cx(0),  cx(0),  cx(0)},
+	            { cx(0),  cx(1),  cx(0),  cx(0),  cx(0),  cx(0),  cx(0),  cx(0)},
+	            { cx(0),  cx(0),  cx(1),  cx(0),  cx(0),  cx(0),  cx(0),  cx(0)},
+	            { cx(0),  cx(0),  cx(0),  cx(1),  cx(0),  cx(0),  cx(0),  cx(0)},
+	            { cx(0),  cx(0),  cx(0),  cx(0),  cx(1),  cx(0),  cx(0),  cx(0)},
+	            { cx(0),  cx(0),  cx(0),  cx(0),  cx(0),  cx(1),  cx(0),  cx(0)},
+	            { cx(0),  cx(0),  cx(0),  cx(0),  cx(0),  cx(0),  cx(0),  cx(1)},
+	            { cx(0),  cx(0),  cx(0),  cx(0),  cx(0),  cx(0),  cx(1),  cx(0)}
+	});
+}
