871a079a877b377c9b7c98699233c8af0c306ada
Andrzej Nieradka <andnie@stud.ics.p.lodz.pl>
2008-05-14 06:40:13 +0000

    Added a file remotely
    git-svn-id: file:///home/rob/www/git/svn-old/jqcomp@25 a4478342-f4fd-11dc-9e01-91044b1c638e

---
+++ PhaseShift.java
@@ -0,0 +1,21 @@
+package pl.lodz.p.ics.quantum.jqcomp;
+
+import org.jscience.mathematics.vector.ComplexMatrix;
+
+public class PhaseShift extends AbstractQGate {
+
+	public PhaseShift(ComplexMatrix matrix) {
+		super(matrix);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PhaseShift(QGate other) {
+		super(other);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PhaseShift() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
