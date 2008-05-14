c29e068eaab7426fcba4df4e773b871de6a063eb
Andrzej Nieradka <andnie@stud.ics.p.lodz.pl>
2008-05-14 04:20:05 +0000

    git-svn-id: file:///home/rob/www/git/svn-old/jqcomp@17 a4478342-f4fd-11dc-9e01-91044b1c638e

---
+++ Identity.java
@@ -0,0 +1,9 @@
+package pl.lodz.p.ics.quantum.jqcomp;
+
+import org.jscience.mathematics.vector.ComplexMatrix;
+
+public class Identity extends AbstractQGate {
+	public Identity(int size) {
+		
+	}
+}
