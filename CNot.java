ba0ef4711e829b91d0b213c51e4af0acf011e82b
Andrzej Nieradka <andnie@stud.ics.p.lodz.pl>
2008-05-14 06:39:52 +0000

    Added a file remotely
    git-svn-id: file:///home/rob/www/git/svn-old/jqcomp@23 a4478342-f4fd-11dc-9e01-91044b1c638e

---
+++ CNot.java
@@ -0,0 +1,21 @@
+package pl.lodz.p.ics.quantum.jqcomp;
+
+import org.jscience.mathematics.vector.ComplexMatrix;
+
+public class CNot extends AbstractQGate {
+
+	public CNot(ComplexMatrix matrix) {
+		super(matrix);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CNot(QGate other) {
+		super(other);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CNot() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
