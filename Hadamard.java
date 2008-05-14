7c869b4687d575469e49d1bf9ba3c421eaecffa5
Andrzej Nieradka <andnie@stud.ics.p.lodz.pl>
2008-05-14 04:19:55 +0000

    git-svn-id: file:///home/rob/www/git/svn-old/jqcomp@16 a4478342-f4fd-11dc-9e01-91044b1c638e

---
+++ Hadamard.java
@@ -0,0 +1,19 @@
+package pl.lodz.p.ics.quantum.jqcomp;
+
+import org.jscience.mathematics.vector.ComplexMatrix;
+
+public class Hadamard extends AbstractQGate {
+	public Hadamard(ComplexMatrix matrix) {
+		super(matrix);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Hadamard(QGate other) {
+		super(other);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Hadamard() {
+		// TODO Auto-generated constructor stub
+	}
+}
