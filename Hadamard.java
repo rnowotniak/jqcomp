432f57f55efa16ece7d78d18e428b57d58bd7d09
Andrzej Nieradka <andnie@stud.ics.p.lodz.pl>
2008-05-14 05:41:07 +0000

    git-svn-id: file:///home/rob/www/git/svn-old/jqcomp@21 a4478342-f4fd-11dc-9e01-91044b1c638e

--- Hadamard.java
+++ Hadamard.java
@@ -3,13 +3,18 @@ package pl.lodz.p.ics.quantum.jqcomp;
 import org.jscience.mathematics.vector.ComplexMatrix;
 import org.jscience.mathematics.number.Complex;
 
-// W³aœciwie nie widze potrzeby robienia oddzielnych klas pod warunkiem 
-// ¿e zachowamy 'niezmiennoœæ' QGate; mo¿na by zrobiæ sta³e statyczne.
+
 public class Hadamard extends AbstractQGate {
-	public Hadamard() {
-		ComplexMatrix m = h2;
-		
-		/* hmm
+	public Hadamard(int size) {
+		 ComplexMatrix m = h2;		
+		 for(int i = 0; i < size - 1; i++) {
+			 m = m.tensor(h2);
+		 }
+		 
+		 this.matrix = m;
+		 this.size = m.getNumberOfRows();		 
+		 
+		/* nie wiem czy dobrze to zinterpretowa³em
 		 * for i in xrange(size - 1):
             m = kron(m, h2)
 		 */		
@@ -18,5 +23,5 @@ public class Hadamard extends AbstractQGate {
 	private static final ComplexMatrix h2 = ComplexMatrix.valueOf(new Complex[][] {
 			{Complex.valueOf(1, 0), Complex.valueOf(1, 0)},
 			{Complex.valueOf(1, 0), Complex.valueOf(-1, 0)}
-	}).times(Complex.valueOf(Math.sqrt(2) / 2, 0));	
+	}).times(Complex.valueOf(Math.sqrt(2) / 2, 0));		
 }
--- Identity.java
+++ Identity.java
@@ -1,9 +1,18 @@
 package pl.lodz.p.ics.quantum.jqcomp;
 
 import org.jscience.mathematics.vector.ComplexMatrix;
+import org.jscience.mathematics.number.Complex;
 
 public class Identity extends AbstractQGate {
 	public Identity(int size) {
+		Complex[][] values = new Complex[size][size];
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				values[i][j] = Complex.valueOf((i == j) ? 1 : 0, 0);
+			}			
+		}
 		
-	}
+		this.matrix = ComplexMatrix.valueOf(values);
+		this.size = this.matrix.getNumberOfRows();
+	}	
 }
