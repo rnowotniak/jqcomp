461689302fb68ac2f6ae2b07acc2783f6baf9762
Robert Nowotniak <robert@nowotniak.com>
2008-05-20 08:18:00 +0000

    Komentarz do algorytmu superdense
    git-svn-id: file:///home/rob/www/git/svn-old/jqcomp@49 a4478342-f4fd-11dc-9e01-91044b1c638e

--- src/pl/lodz/p/ics/quantum/jqcomp/algorithms/Superdense.java
+++ src/pl/lodz/p/ics/quantum/jqcomp/algorithms/Superdense.java
@@ -8,6 +8,22 @@ import pl.lodz.p.ics.quantum.jqcomp.PhaseShift;
 import pl.lodz.p.ics.quantum.jqcomp.QGate;
 import pl.lodz.p.ics.quantum.jqcomp.QRegister;
 
+/**
+ * Superdense coding example
+ * 
+ * Superdense coding allows to encode one of four classical states
+ * (00, 01, 10, 11) in one quantum bit of information.
+ * What unique phenomenon exists in this operation? 
+ * The measurement performed on the two-level quantum system (qubit) can yield
+ * one of only two values: for e.g. horizontal or vertical polarization of photon.
+ * Despite that, one of four states can be encoded and extracted from such
+ * two-level quantum system, by the use of quantum channel (entangled EPR pair).
+ * 
+ * @author rob
+ * 
+ * TODO: Use Dirac notation, as soon as provided by QRegister
+ * 
+ */
 public class Superdense {
 
 	public static void main(String[] args) {
@@ -25,7 +41,11 @@ public class Superdense {
 		System.out.println("Initial state (should be the EPR pair");
 		System.out.println(qreg);
 		
-		// Conditional operations on the ONE qubit in the entangled pair
+		//
+		// << One qubits from the pair goes to Alice, the second to Bob >>
+		//
+		
+		// Conditional operations on the ONE qubit from the entangled pair
 		if (b0 != 0) {
 			qreg = (new PhaseShift(Math.PI)).next(new Identity()).compute(qreg);
 		}
@@ -33,6 +53,11 @@ public class Superdense {
 			qreg = (new Not()).next(new Identity()).compute(qreg);
 		}
 
+		//
+		// << Alice sends her qubit to Bob >>
+		//
+		
+		// Decoding matrix
 		QGate B = new Arbitrary(MoreMath.asComplexMatrix(new double[][] {
 				{ s2, 0, 0, s2 },
 				{ 0, s2, s2, 0 },
@@ -40,7 +65,9 @@ public class Superdense {
 				{ 0, -s2, s2, 0 }
 		}));
 
+		// Perform operation on both qubits
 		QRegister result = B.compute(qreg);
+		
 		System.out.println("Final state:");
 		System.out.println("(should be one of the four base vectors, according to b0, b1 bits:");
 		System.out.println(result);
